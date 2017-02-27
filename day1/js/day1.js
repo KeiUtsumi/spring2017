c5512ab5727743b6c88bc3b5b70913d535c0d79d
KeiUtsumi <st061660@m01.kyoto-kcg.ac.jp>
2017-02-27 15:08:08 +0900

    2 commit
    js

---
+++ day1/js/day1.js
@@ -0,0 +1,19 @@
+console.log("はい");
+
+var main = document.getElementsByClassName("main");
+main[0].insertAdjacentHTML('afterbegin', '<p>Learning JavaScript</p>');
+
+var Cat = function(name) {
+    this.name = name
+    /*this.run = function() {//いちいち生成させるのが良くないので
+        console.log(this.name + "が走る");
+    };*/
+};
+Cat.prototype.run = function() {
+    console.log(this.name + "が走る");
+};
+
+var tama = new Cat('タマ');
+tama.run();
+console.log(tama.name);
+
